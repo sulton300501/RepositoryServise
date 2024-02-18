53539b0106a3818fa10d10946ded175b81d7c45a
sulton <sultonmalik326@gmail.com>
2024-02-19 00:08:27 +0500

    malumot

---
+++ WebApplication6/MyPattern/IRepositories/ITeacherRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication6.Entities.DTOs;
+using WebApplication6.Models;
+
+namespace WebApplication6.MyPattern.IRepositories
+{
+    public interface ITeacherRepository
+    {
+
+        public string CreateTeacher(TeachedDTO teacherdto);
+
+        public IEnumerable<Teacher> GetAllTeachers();
+
+
+        public bool DeleteTeacher(int id);
+        public Teacher UpdateTeacher(int id, TeachedDTO teacherdto);
+
+
+
+
+    }
+}
---
+++ WebApplication6/MyPattern/Repositories/TeacherRepository.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using WebApplication6.Entities.DTOs;
+using WebApplication6.Models;
+using WebApplication6.MyPattern.IRepositories;
+
+namespace WebApplication6.MyPattern.Repositories
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public TeacherRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateTeacher(TeachedDTO teacherdto)
+        {
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    string query = "";
+
+                    var parameters = new TeachedDTO
+                    {
+                        full_name=teacherdto.full_name,
+                        age=teacherdto.age,
+                        salary=teacherdto.salary,
+                        phone=teacherdto.phone,
+                        groups_count=teacherdto.groups_count,
+                        experience=teacherdto.experience,
+                       
+                       
+                    };
+
+                    connection.ExecuteAsync(query, parameters);
+
+                    // Return the updated student
+                  
+                }
+                return "Malumot yaratildi";
+            }
+            catch (Exception)
+            {
+                // Handle update failure
+                return null;
+            }
+
+
+        }
+
+        public bool DeleteTeacher(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Teacher> GetAllTeachers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Teacher UpdateTeacher(int id, TeachedDTO teacherdto)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
+
